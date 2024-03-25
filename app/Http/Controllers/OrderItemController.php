070933473752fa7250a93245f44359f087c6e4d5
shohruh <shohruh123777@gmail.com>
2024-03-25 11:21:49 +0500

    init project and add migration

---
+++ app/Http/Controllers/OrderItemController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order_Item;
+use Illuminate\Http\Request;
+
+class OrderItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Order_Item $order_Item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Order_Item $order_Item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Order_Item $order_Item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Order_Item $order_Item)
+    {
+        //
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'table_id',
+        'total_price',
+        'status',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Order_item::class);
+    }
+}
